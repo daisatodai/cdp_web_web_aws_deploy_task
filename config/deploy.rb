8ba72ffda10483a47b93714d6e89962a92fb8137
dai <sugerbig15@gmail.com>
2024-04-18 16:50:11 +0900

    add capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,31 @@ lock "~> 3.18.1"
 
 set :application, "cdp_web_web_aws_deploy_task"
 set :repo_url, "https://github.com/daisatodai/cdp_web_web_aws_deploy_task.git"
-set :bundle_without, %w{test}.join(':')
-
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
+set :keep_releases, 5 
 set :rbenv_version, '3.0.1'
 set :linked_files, %w{config/secrets.yml .env}
+set :log_level, :info
+
+after 'deploy:published', 'deploy:seed'   # 9
+ after 'deploy:finished', 'deploy:restart'   # 10
+
+ namespace :deploy do
+   desc 'Run seed'
+   task :seed do
+     on roles(:db) do
+       with rails_env: fetch(:rails_env) do
+         within current_path do
+           execute :bundle, :exec, :rake, 'db:seed'
+         end
+       end
+     end
+   end
+   desc 'Restart application'
+   task :restart do
+     invoke 'unicorn:restart'
+   end
+ end
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
